b3409c0b8bb9b9ce622ab8d1335604fa11857b6a
Lee Chuan Xin <chuanxin.lee@gmail.com>
2017-03-02 05:35:31 +0800

    Added "ANS" button

--- js/main.js
+++ js/main.js
@@ -8,10 +8,30 @@
 // test "0/0"
 // test "0.3 + 0.6"
 // test "0.6 / 3"
+// test "-3" right after hitting equal
+// test "9 x -3"
+// add replacer for trailing zeros
+// add error notification for NaN and infinity
+
+// "9 + 9 ="
+// input display: ''
+// 
+// "."
+// input display: '.'
+// 
+
+// "9 + 9 ="
+// ans = 18
+// "x"
+// nothing happens
+// 
+
 
 var test;
 
-console.log(test == undefined);
+// console.log(test == undefined);
+
+// console.log(eval("1- - -  - - -1"));
 
 // Buttons
 var buttons = (function() {
@@ -42,13 +62,14 @@ var buttons = (function() {
 // Display
 var display = (function() {
 	// cache DOM
-	var input = document.querySelector('.input'),
+	var	input = document.querySelector('.input'),
 		answer = document.querySelector('.answer');
 
 	// init variables
-	var inputText = '',
+	var	inputText = '',
 		answerNum = 0,
 		answerText = 'ans'.toUpperCase(),
+		isEvaluated = true,
 		limitWarning = 'Limit Reached!'.toUpperCase(),
 		maxDisplayLength = 12;
 
@@ -62,7 +83,8 @@ var display = (function() {
 
 	// add input
 	function addInput(newInput) {
-		var previousInput = inputText[inputText.length - 1],
+		// init indexes
+		var	previousInput = inputText[inputText.length - 1],
 			divideLastIndex = inputText.lastIndexOf('/'),
 			minusLastIndex = inputText.lastIndexOf('-'),
 			plusLastIndex = inputText.lastIndexOf('+'),
@@ -70,31 +92,41 @@ var display = (function() {
 			operatorLastIndexArr = [divideLastIndex, minusLastIndex, plusLastIndex, timesLastIndex];
 
 		// init basic conditions
-		var hasDecimalPointAfterLastOperator = (inputText.lastIndexOf('.') > Math.max.apply(null, operatorLastIndexArr)),
-			isDecimalPointOrOperator = function(str) {
-				return util.isDecimalPoint(str) || util.isOperator(str);
-			};
+		var	hasDecimalPointAfterLastOperator = (inputText.lastIndexOf('.') > Math.max.apply(null, operatorLastIndexArr)),
+			isPreviousInputAnswer = (inputText.slice(inputText.length - answerText.length) == answerText);	
 			
 		// init complex conditions
-		var digitAfterAnswer = util.isAnswer(inputText) && (util.isNumber(newInput) || util.isDecimalPoint(newInput)),
-			multipleDecimalPoints = util.isDecimalPoint(newInput) && hasDecimalPointAfterLastOperator,
-			notNumberAfterDecimalPoint = util.isDecimalPoint(previousInput) && !util.isNumber(newInput),
-			operatorAfterOperator = util.isOperator(previousInput) && util.isOperator(newInput),
-			rejectedConditions = digitAfterAnswer || multipleDecimalPoints || notNumberAfterDecimalPoint || operatorAfterOperator;
+		var	hasDigitAfterAnswer = isPreviousInputAnswer && util.isAnswerOrDecimalPointOrNumber(newInput),
+			hasDigitBeforeAnswer = util.isAnswer(newInput) && (util.isDecimalPointOrNumber(previousInput) || isPreviousInputAnswer),
+			displayAnswerBeforeMinus = util.isMinus(newInput) && (isEvaluated == true) && (answerNum != 0),
+			hasMultipleDecimalPoints = util.isDecimalPoint(newInput) && hasDecimalPointAfterLastOperator,
+			hasNotNumberAfterDecimalPoint = util.isDecimalPoint(previousInput) && !util.isNumber(newInput),
+			hasOperatorAfterOperator = util.isOperator(previousInput) && util.isNotMinusOperator(newInput),
+			rejectedConditions = hasDigitAfterAnswer || hasDigitBeforeAnswer || hasMultipleDecimalPoints || hasNotNumberAfterDecimalPoint || hasOperatorAfterOperator;
 
-		// add 'ans' text if new input is operator and previous input is empty
-		if (util.isOperator(newInput) && inputText == '') {
-			renderInput(answerText + newInput);
+		// add 'ans' text for all operators (except minus)
+		// if minus, add 'ans' if current answer != 0 and input has just been evaluated
+		if (inputText == '') {
+			if (util.isNotMinusOperator(newInput) || displayAnswerBeforeMinus) {
+				renderInput(answerText + newInput);
+			}
+			else {
+				renderInput(inputText + newInput);
+			}
 		}
 		else if (!rejectedConditions) {
 			renderInput(inputText + newInput);
 		}
+		
+		// current input not evaluated yet; set isEvaluated to false
+		isEvaluated = false;
 	};
 
 	// clear all
 	function clearAll() {
 		inputText = '';
 		answerNum = 0;
+		isEvaluated = true;
 
 		renderInput();
 		renderAnswer();
@@ -108,7 +140,7 @@ var display = (function() {
 		if (util.isOperator(previousInput)) {
 			inputText = inputText.slice(0, inputText.length - 1);
 		}
-		// if number or "ans", clear its entire entry until next operator or empty string
+		// if not operator, clear its entire entry until next operator or empty string
 		else {
 			while (!util.isOperator(previousInput) && previousInput != undefined) {
 				inputText = inputText.slice(0, inputText.length - 1);
@@ -121,22 +153,35 @@ var display = (function() {
 
 	// equal
 	function equal() {
-		var previousInput = inputText[inputText.length - 1];
+		// init regex
+		var	ansRegex = new RegExp("(" + answerText + ")", "g"),
+			timesRegex = new RegExp("x", "g"),
+			trailingMinusRegex = new RegExp("(-{2,})", "g");
 
-		// evaluates only if last input entry is number or answer
-		if (util.isNumber(previousInput) || util.isAnswer(inputText)) {
-			var timesRegex = /x/g;
+		// init replacer functions
+		var trailingMinusReplacer = function(match) {
+			return match.split('').join(' ');
+		};
 
-			// convert "ANS" string to actual number for calc
-			if (inputText.indexOf(answerText) == 0) {
-				inputText = answerNum.toString() + inputText.slice(answerText.length);
-			}
+		// replace all "ans" and 'x' and trailing minuses
+		var tempInput = inputText.replace(ansRegex, answerNum.toString())
+								 .replace(timesRegex, "*")
+								 .replace(trailingMinusRegex, trailingMinusReplacer);
 
-			// evaluates answer from converted input text
-			renderAnswer(eval(inputText.replace(timesRegex, "*")).toString());
+		// init previous input
+		var previousInput = tempInput[tempInput.length - 1];
+
+		// evaluates only if last input entry is number
+		if (util.isNumber(previousInput)) {
+			// eval answer
+			renderAnswer(eval(tempInput).toString());
 
 			// clear all input
 			inputText = '';
+
+			// set new calculation
+			isEvaluated = true;
+
 			renderInput();
 		}
 	};
@@ -170,7 +215,7 @@ var display = (function() {
 
 	// render answer
 	function renderAnswer(str) {
-		var currentAnswerDisplay = answer.textContent,
+		var	currentAnswerDisplay = answer.textContent,
 			nextAnswerDisplay = str || answerNum.toString();
 
 		if (nextAnswerDisplay.length <= maxDisplayLength) {
@@ -184,7 +229,7 @@ var display = (function() {
 
 	// render input
 	function renderInput(str) {
-		var currentInputDisplay = input.textContent,
+		var	currentInputDisplay = input.textContent,
 			nextInputDisplay = str || inputText;
 
 		if (nextInputDisplay.length <= maxDisplayLength) {
@@ -231,9 +276,39 @@ var util = (function() {
 		return (str == display.getAnswerText());
 	};
 
+	// checks if string is answer or number or decimal point
+	function isAnswerOrDecimalPointOrNumber(str) {
+		return isAnswer(str) || isDecimalPoint(str) || isNumber(str);
+	};
+
 	// checks if string is decimal point
 	function isDecimalPoint(str) {
 		return (str == '.');
+	};
+
+	// checks if string is decimal point or number
+	function isDecimalPointOrNumber(str) {
+		return isDecimalPoint(str) || isNumber(str);
+	};
+
+	// checks if string is decimal point or operator
+	function isDecimalPointOrOperator(str) {
+		return isDecimalPoint(str) || isOperator(str);
+	};
+
+	// checks if string is divide
+	function isDivide(str) {
+		return (str == '/');
+	};
+
+	// checks if string is minus
+	function isMinus(str) {
+		return (str == '-');
+	};
+
+	// checks if string is an operator, but not minus
+	function isNotMinusOperator(str) {
+		return (isTimes(str) || isDivide(str) || isPlus(str));
 	};
 
 	// checks if string is number
@@ -241,14 +316,29 @@ var util = (function() {
 		return !isNaN(Number(str));
 	};
 
+	// checks if string is plus
+	function isPlus(str) {
+		return (str == '+');
+	};
+
+	// checks if string is times
+	function isTimes(str) {
+		return (str == 'x');
+	};
+
 	// checks if string is operator
 	function isOperator(str) {
-		return (str == 'x' || str == '/' || str == '+' || str == '-');
+		return (isTimes(str) || isDivide(str) || isPlus(str) || isMinus(str));
 	};
 
 	return {
 		isAnswer: isAnswer,
+		isAnswerOrDecimalPointOrNumber: isAnswerOrDecimalPointOrNumber,
 		isDecimalPoint: isDecimalPoint,
+		isDecimalPointOrNumber: isDecimalPointOrNumber,
+		isDecimalPointOrOperator: isDecimalPointOrOperator,
+		isMinus: isMinus,
+		isNotMinusOperator: isNotMinusOperator,
 		isNumber: isNumber,
 		isOperator: isOperator
 	};
